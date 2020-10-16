7d152b49aaa634c72d8bba2688c26b14a941a5c6
Marcos Hobson <marcoshobson@gmail.com>
2020-10-16 15:51:15 -0300

    Implementando o recurso de listar todos os empregados

---
+++ src/main/java/com/mhdev/springbootcrud/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.mhdev.springbootcrud.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mhdev.springbootcrud.model.Employee;
+import com.mhdev.springbootcrud.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	// get all employees
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees() {
+		return employeeRepository.findAll();
+	}
+}
