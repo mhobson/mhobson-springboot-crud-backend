18382bbc78f55b18a39db0c58634c8e8b5b541a3
Marcos Hobson <marcoshobson@gmail.com>
2020-10-20 16:47:18 -0300

    Implementação do recurso para adicionar novo empregado

--- src/main/java/com/mhdev/springbootcrud/controller/EmployeeController.java
+++ src/main/java/com/mhdev/springbootcrud/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,16 +14,22 @@ import com.mhdev.springbootcrud.model.Employee;
 import com.mhdev.springbootcrud.repository.EmployeeRepository;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/")
 public class EmployeeController {
 	
 	@Autowired
 	private EmployeeRepository employeeRepository;
 	
-	// get all employees
-	@CrossOrigin(origins = "http://localhost:4200")
+	// Get all employees
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployees() {
 		return employeeRepository.findAll();
+	}
+	
+	// Create a employee
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
 	}
 }
