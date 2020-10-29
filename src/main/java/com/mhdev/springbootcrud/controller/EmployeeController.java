2c37d0e08214192d6cb7b30b7b8f9f1b8031e2cb
Marcos Hobson <marcoshobson@gmail.com>
2020-10-29 10:28:50 -0300

    Implementação do recurso getEmployeeByID para buscar um empregado pelo id

--- src/main/java/com/mhdev/springbootcrud/controller/EmployeeController.java
+++ src/main/java/com/mhdev/springbootcrud/controller/EmployeeController.java
@@ -3,13 +3,16 @@ package com.mhdev.springbootcrud.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mhdev.springbootcrud.exception.ResourceNotFoundException;
 import com.mhdev.springbootcrud.model.Employee;
 import com.mhdev.springbootcrud.repository.EmployeeRepository;
 
@@ -25,6 +28,15 @@ public class EmployeeController {
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployees() {
 		return employeeRepository.findAll();
+	}
+	
+	// Get employee by id
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+		return ResponseEntity.ok(employee);
+		
 	}
 	
 	// Create a employee
